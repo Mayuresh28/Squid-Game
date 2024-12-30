530f5d5fb7f406c488394de9d1b42225c30f9a85
Mayuresh Muluk <92907614+Mayuresh28@users.noreply.github.com>
2024-12-30 23:48:17 +0530

    Update settings.py

--- squidgame/settings.py
+++ squidgame/settings.py
@@ -125,5 +125,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
